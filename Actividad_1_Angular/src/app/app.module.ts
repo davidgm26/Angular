634066e37e92c17852c53cffa784fbc34c3e5988
davidgm26 <davidgama260402@gmail.com>
2022-09-28 16:59:11 +0200

    Add all standard components

--- Actividad_1_Angular/src/app/app.module.ts
+++ Actividad_1_Angular/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
