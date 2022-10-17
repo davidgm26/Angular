fa98fcac448098f8038143808a7e9d90ff989586
davidgm26 <davidgama260402@gmail.com>
2022-10-18 00:48:29 +0200

    no consigo mostrar el seleccionado

--- Pokemon/src/app/components/pokemon-list/pokemon-list.component.ts
+++ Pokemon/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PokemonInfoDialogComponent } from 'src/app/dialogs/pokemon-info-dialog/pokemon-info-dialog.component';
 import { Pokemon } from 'src/app/interfaces/pokemon-response.interface';
+import { PokemonDetailResponse } from 'src/app/interfaces/pokemons-detail-response.interface';
 import { PokemonServiceService } from 'src/app/services/pokemon-service.service';
 
 @Component({
@@ -9,8 +12,10 @@ import { PokemonServiceService } from 'src/app/services/pokemon-service.service'
 })
 export class PokemonListComponent implements OnInit {
   listadoPokemon: Pokemon[] = [];
+  pokemonSelected: PokemonDetailResponse | undefined;
 
-  constructor(private pokemonService: PokemonServiceService) {}
+
+  constructor(private pokemonService: PokemonServiceService,public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.pokemonService.pokemonList().subscribe((response) => {
@@ -20,6 +25,18 @@ export class PokemonListComponent implements OnInit {
   public getPhotoURL(p: Pokemon): String {
     return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
       p.url.split('/')[6]}.png`;
+  }
+
+  getPokemonInfo(pokemon: Pokemon) {
+    this.pokemonService.getPokemonDetail(pokemon).subscribe(response => {
+      this.pokemonSelected = response;
+      this.dialog.open(PokemonInfoDialogComponent, {
+        data: {
+          pokemonInfo: this.pokemonSelected,
+          color: '#FF0000'
+        },
+      });
+    });
   }
 
 }
